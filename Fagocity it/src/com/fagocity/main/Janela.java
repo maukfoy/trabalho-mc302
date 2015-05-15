6acfec9820c7eccf01f0c22db38e69f3de9c5c99
PedroBarcha <correia.barcha@gmail.com>
2015-05-15 01:51:09 -0300

    Update Janela.java

--- Fagocity it/src/com/fagocity/main/Janela.java
+++ Fagocity it/src/com/fagocity/main/Janela.java
@@ -9,15 +9,14 @@ public class Janela extends Canvas {
 
 	private static final long serialVersionUID = -7847704263629313879L;
 	
-	/* Método que configura a janela do jogo */
+	/* MÃ©todo que configura a janela do jogo */
 	public Janela (int altura, int largura, String titulo, Fagocity jogo) {
 		JFrame frame = new JFrame(titulo);
-		Dimension tamanho = new Dimension(largura, altura);
 		
 		/* Atributos da janela */
-		frame.setPreferredSize(tamanho);
-		frame.setMinimumSize(tamanho);
-		frame.setMaximumSize(tamanho);
+		frame.setPreferredSize(new Dimension(largura, altura));
+		frame.setMaximumSize(new Dimension(largura, altura));
+		frame.setMinimumSize(new Dimension(largura, altura));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//botao de fechar encerra o jogo
 		frame.setResizable(false);//janela tem tamanho unico
 		frame.setLocationRelativeTo(null);//janela centralizada
