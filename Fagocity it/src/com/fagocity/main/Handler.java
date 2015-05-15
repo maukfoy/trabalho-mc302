5836d63a16ee2677a181dc161a415a5e7c66176c
PedroBarcha <correia.barcha@gmail.com>
2015-05-15 01:49:17 -0300

    Update Handler.java

--- Fagocity it/src/com/fagocity/main/Handler.java
+++ Fagocity it/src/com/fagocity/main/Handler.java
@@ -7,33 +7,39 @@ import java.util.LinkedList;
 public class Handler {
 	
 	/*cria lista contendo todos os objetos*/
-	LinkedList<ObjetoJogo> obj = new LinkedList<ObjetoJogo>();
+	LinkedList<Objeto> obj = new LinkedList<Objeto>();
 	
-	/* Método que percorre todos objetos e atualiza suas informações */
-	public void tick() {
+	/* MÃ©todo que percorre todos objetos e atualiza suas informaÃ§Ãµes */
+	public void tick() 
+	{
 		int i;
-		for( i = 0; i < obj.size(); i++ ) {
-			ObjetoJogo tempObj= obj.get(i);
+		for( i = 0; i < obj.size(); i++ )
+		{
+			Objeto tempObj= obj.get(i);
 			tempObj.tick();
 		}
 	}
 	
-	/* Método que percorre todos objetos, renderiza e os coloca na tela */
-	public void render(Graphics g) {
+	/* MÃ©todo que percorre todos objetos, renderiza e os coloca na tela */
+	public void render(Graphics g)
+	{
 		int i;
-		for( i = 0; i < obj.size(); i++ ) {
-			ObjetoJogo tempObj= obj.get(i);
+		for( i = 0; i < obj.size(); i++ )
+		{
+			Objeto tempObj= obj.get(i);
 			tempObj.render(g);
 		}
 	}
 	
-	/* Adiciona um objeto à lista ligada de objetos do jogo */
-	public void addObjeto(ObjetoJogo objeto) {
+	/* Adiciona um objeto Ã  lista ligada de objetos do jogo */
+	public void addObjeto(Objeto objeto) 
+	{
 		this.obj.add(objeto);
 	}
 	
 	/* Remove um objeto da lista ligada de objetos do jogo */
-	public void delObjeto(ObjetoJogo objeto) {
+	public void delObjeto(Objeto objeto)
+	{
 		this.obj.remove(objeto);
 	}
 
