b87992bc938359b23b95d8a01f6fd27f43219956
PedroBarcha <correia.barcha@gmail.com>
2015-05-15 01:50:03 -0300

    Update HUD.java

--- Fagocity it/src/com/fagocity/main/HUD.java
+++ Fagocity it/src/com/fagocity/main/HUD.java
@@ -5,22 +5,30 @@ import java.awt.Graphics;
 
 public class HUD {
 
-	public static int vida = 100;
-
+	/*static pois o jogador so tem uma vida*/
+	public static float vida = 100;
 	
-	public void tick(){
+	public void tick()
+	{
 		/*define os limites da barra de vida*/ 
 		vida = Fagocity.limita (vida, 0, 100);
 		
 	}
 	
-	public void render(Graphics g){
+	public void render(Graphics g)
+	{
+		/*vida total inicial*/
 		g.setColor(Color.gray);
 		g.fillRect(15, 15, 200, 32);
+		
+		/*quantidade restante de vida*/
 		g.setColor(Color.green);
-		g.fillRect(15, 15, vida * 2, 32);
+		g.fillRect(15, 15, (int) vida * 2, 32);
+		
+		/*borda da barra*/
 		g.setColor(Color.white);
 		g.drawRect(15, 15, 200, 32);
+		
 	}
 	
 }
