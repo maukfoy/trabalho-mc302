d6b54d378d37a2f85ee393c3648d88bcae8614e7
maukfoy <mauricio.lorenzetti.bezerra@gmail.com>
2015-05-24 22:27:57 -0300

    Só fiquei zuando
    zoas, padronizei o tempo no game loop e tentei desenhar uma suastica

--- Fagocity it/src/fagocity/GameMain.java
+++ Fagocity it/src/fagocity/GameMain.java
@@ -5,6 +5,8 @@ import fagocity.model.GameModel;
 import fagocity.view.GameView;
 
 public class GameMain implements Runnable {
+	
+	private static final int MilisToSecs = 1000;
 	private GameView view;
 	private GameModel model;
 	private boolean running;
@@ -14,13 +16,11 @@ public class GameMain implements Runnable {
 		new GameMain();
 	}
 	
-	GameMain() {
+	public GameMain() {
 		/* Cria o Model e o View */
 		this.model = new GameModel();
 		this.view = new GameView(model);
-		
-		/* Abre a janela principal */
-		this.view.CriaJanelaPrincipal();
+		view.setVisible(true);
 		
 		/* Cria a Thread */
 		this.thread = new Thread(this);
@@ -29,9 +29,9 @@ public class GameMain implements Runnable {
 
 	
 	public void run() {
-		long lastTime = System.nanoTime();
+		long lastTime = System.currentTimeMillis();
 		final double amountOfTicks = 60.0;
-		double ns  = 1000000000 / amountOfTicks;
+		double ms  = MilisToSecs / amountOfTicks;
 		double delta = 0;
 		int updates = 0;
 		int frames = 0;
@@ -40,22 +40,32 @@ public class GameMain implements Runnable {
 		running = true;
 		/* Game Loop */
 		while(running) {
-			long now = System.nanoTime();
-			delta += (now - lastTime) / ns;
+			long now = System.currentTimeMillis();
+			delta += (now - lastTime) / ms;
 			lastTime = now;
 			if(delta >= 1) {
+				
+				/* ******************************************** */
+				/*												*/
 				/* CHAMAR AQUI O CONTROLLER PARA FAZER OS TICKS */
+				/*												*/
+				/* ******************************************** */
+				
 				//tick();
 				updates++;
 				delta--;
 			}
-			/* CHAMAR AQUI O VIEW PARA EXIBIR A TELA */
+			/* ******************************************** */
+			/*												*/
+			/*	CHAMAR AQUI O VIEW PARA EXIBIR A TELA		*/
+			/*												*/
+			/* ******************************************** */
 			//render();
-			frames ++;
+			frames++;
 			
 			if(System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
-				System.out.println(updates + "Ticks, Fps" + frames);
+				System.out.println(updates + " Ticks, Fps " + frames);
 				updates = 0;
 				frames = 0;
 			}
--- Fagocity it/src/fagocity/view/GameView.java
+++ Fagocity it/src/fagocity/view/GameView.java
@@ -2,6 +2,7 @@ package fagocity.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,31 +13,32 @@ import fagocity.model.GameModel;
 public class GameView extends JFrame {
 	
 	private static final int WIDTH = 800;
-	private static final int HEIGHT = 600;
+	private static final int HEIGHT = WIDTH / 4 * 3; // Força aspect ratio 4:3 baseado no WIDTH
 	
 	private GameModel model;
 	private JPanel screen;
-	private boolean visible;
 
 	public GameView(GameModel model) {
 		this.model = model;
-		this.visible = false;
+		SetMainWindow();
 	}
 	
 	/* Cria a janela principal */
-	public void CriaJanelaPrincipal() {
-		if(visible == false) {
-			screen = new JPanel();
-			screen.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-			screen.setBackground(Color.BLUE);
-			add(screen);
-			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			pack();
-			setLocationRelativeTo(null);
-			this.visible = true;
-			this.setVisible(true);
-		}
+	public void SetMainWindow() {
+		setTitle("Fagocity It!");
+		screen = new JPanel();
+		screen.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		screen.setBackground(Color.RED);
+		add(screen);
+		
+		JPanel screen1 = new JPanel();
+		screen1.setPreferredSize(new Dimension(70, HEIGHT));
+		screen1.setBackground(Color.BLACK);
+		screen.add(screen1);
+		
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
 	}
-	
-	
 }
