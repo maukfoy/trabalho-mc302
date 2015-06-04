69b71d86b9f94ca4a90d0a6e18a1e11afa3b1d8b
PedroBarcha <correia.barcha@gmail.com>
2015-06-04 05:23:03 -0300

    Update ActorFactory.java

--- Fagocity it/src/fagocity/controller/ActorFactory.java
+++ Fagocity it/src/fagocity/controller/ActorFactory.java
@@ -1,33 +1,32 @@
 package fagocity.controller;
 
 import java.awt.Color;
+import java.util.EmptyStackException;
 
 import fagocity.model.Enemy;
 import fagocity.model.Player;
-import fagocity.model.interfaces.IActor;
 
 public class ActorFactory {
 	
 	/* Cria um Actor */
-	public static IActor createActor(int x, int y, double velX, double velY, int radius, Color color, String type) {
+	public static void createActor(double x, double y, double velX, double velY, double radius, Color color, String type) {
 		
 		/* Cria um Player */
 		if( type.equals("player")) {
 			Player player = new Player(x, y, velX, velY, radius, color);
-			/* Adiciona o player ‡ lista de actors */
+			/* Adiciona o player √† lista de actors */
 			GameController.addToActorsList(player);
-			return player;
 		}
 		
 		/* Cria um Enemy */
 		else if( type.equals("enemy")) {
 			Enemy enemy = new Enemy(x, y, velX, velY, radius, color);
-			/* Adiciona o enemy ‡ lista de actors */
+			/* Adiciona o enemy √† lista de actors */
 			GameController.addToActorsList(enemy);
-			return enemy;
 		}
 		
-		/* O objeto pedido para ser criado n„o È conhecido */
-		return null;
+		/* O objeto pedido para ser criado n√£o √© conhecido */
+		else
+			throw new EmptyStackException();
 	}
 }
