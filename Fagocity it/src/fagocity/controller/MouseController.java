fe3db291d44c3789a5a86ce8db5e3c5921a83d2a
PedroBarcha <correia.barcha@gmail.com>
2015-06-04 05:21:31 -0300

    Update MouseController.java

--- Fagocity it/src/fagocity/controller/MouseController.java
+++ Fagocity it/src/fagocity/controller/MouseController.java
@@ -4,23 +4,24 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
-import fagocity.model.interfaces.IActor;
+
+import fagocity.model.Actor;
 
  public class MouseController extends MouseAdapter implements MouseMotionListener {
-	ArrayList<IActor> ActorsList;
-	private static int mousePosX = 0;
-	private static int mousePosY = 0;
+	ArrayList<Actor> ActorsList;
+	private static double mousePosX = 0;
+	private static double mousePosY = 0;
 	
 	public void mouseMoved(MouseEvent e) {
 		mousePosX = e.getX();
 		mousePosY = e.getY();
 	}
 	
-	public static int getMouseX() {
+	public static double getMouseX() {
 		return mousePosX;
 	}
 	
-	public static int getMouseY() {
+	public static double getMouseY() {
 		return mousePosY;
 	}
 }
