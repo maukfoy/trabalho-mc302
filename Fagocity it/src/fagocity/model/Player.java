65723a4625f470e796bad6d7b2dbc1ef782ed4ed
PedroBarcha <correia.barcha@gmail.com>
2015-06-04 05:13:48 -0300

    Update Player.java

--- Fagocity it/src/fagocity/model/Player.java
+++ Fagocity it/src/fagocity/model/Player.java
@@ -2,50 +2,12 @@ package fagocity.model;
 
 import java.awt.Color;
 
-import fagocity.controller.MouseController;
-import fagocity.model.interfaces.IActor;
+import fagocity.controller.PlayerController;
 
-public class Player extends Actor implements IActor {
-	
-	public Player(int x, int y, double velX, double velY, int radius, Color color) {
-		super(x, y, velX, velY, radius, color);
-	}
-	
-	public void update() {
-		AtualizaVelocidade();
-		atualizarPosicao();
-	}
-	
-	private void atualizarPosicao() {
-		x += velX;
-		y += velY;
-	}
-	
-	private void AtualizaVelocidade() {
-		double velTotal = 8;
-		int mouseX = MouseController.getMouseX();
-		int mouseY = MouseController.getMouseY();
+public class Player extends Actor {
 		
-		/* Se houver distância entre x do mouse e x da bola, mover a bola no eixo x */
-		if( (mouseX - x) != 0.0 ) {
-			double oldVelX = velX;
-			velX = velTotal * ( (mouseX - x) / Math.sqrt( (mouseY - y)*(mouseY - y) +  (mouseX - x)*(mouseX - x) ) );
-			if( oldVelX > 0 && velX < 0 ||  oldVelX < 0 && velX > 0) {
-				velX = 0;
-				// x = mouseX; talvez seja util - atualmente da bug, nao usar
-			}
-			System.out.println(velX);
-		}
-		/* Se houver distância entre y do mouse e y da bola, mover a bola no eixo y */
-		if( (mouseY - y) != 0.0 ) {
-			double oldVelY = velY;
-			velY = velTotal * ( (mouseY - y) / Math.sqrt( (mouseY - y)*(mouseY - y) +  (mouseX - x)*(mouseX - x) ) );
-			if( oldVelY > 0 && velY < 0 ||  oldVelY < 0 && velY > 0) {
-				velY = 0;
-				// y = mouseY; talvez seja util - atualmente da bug, nao usar
-			}
-			//System.out.println(velY);
-		}
-	}
-	
+	public Player(double x, double y, double velX, double velY, double radius, Color color) {
+		super(x, y, velX, velY, radius, color);
+		actorController = new PlayerController(this);
+	}	
 }
