fa6f2a6f0d87ce18ee022f216cac2c135284eaea
Vitor <vitoralves842@gmail.com>
2015-06-05 19:46:35 -0300

    minor HUD score bug fixed

--- Fagocity it/src/fagocity/model/Player.java
+++ Fagocity it/src/fagocity/model/Player.java
@@ -7,6 +7,7 @@ import fagocity.controller.PlayerController;
 public class Player extends Actor {
 	public static final int defaultRadius = 30;
 	public static final double defaultSpeed = 8;
+	public static int lives = 3;
 		
 	public Player(int x, int y, double velX, double velY, int radius, Color color) {
 		super(x, y, velX, velY, radius, color);
